090b14d8eeea25e847f735f6ea703f85e3a23e83
motoy3d <motoy3d@gmail.com>
2014-12-10 01:17:30 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'antlers'
+    ,hashtag: 'antlers'
+    ,teamName: '鹿島'
+    ,appName: 'スマートJ for 鹿島アントラーズ'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id921999734?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.antlers'
+    ,color: '#b7193f'
+    ,tintColor: '#bdb1a9'
+    ,dbName: 'smartj.antlers'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=antlers&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=antlers"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 241073
+    ,nendApiKeyIPhoneIcon: '7f52ee7e05fbf731b377f70ba12f3b5db1d899cd'
+    ,nendSpotIdIPhoneBanner: 241072
+    ,nendApiKeyIPhoneBanner: '1e468223efc3450291d32b9fd9fffda4079e77f5'
+    //nend(Android)
+    ,nendSpotIdAndroid: 241071
+    ,nendApiKeyAndroid: '87b5edf1466ef1ba49f7fd9f9c43f36cf1401a8f'
+};
